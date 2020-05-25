a32cf0a86fecf72028a0bc8b56ecc622ad8326ff
furuiyang <furuiyang0715@gmail.com>
2020-05-25 13:35:09 +0800

    获取知乎问答图片

---
+++ zhihu_pics/pic_get.py
@@ -0,0 +1,61 @@
+import json
+import pprint
+import sys
+import time
+
+import requests
+from lxml import html
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
+
+
+
+}
+
+
+def download_picture(url):
+    print(url)
+    pic_name = url.split("/")[-1]
+    print(pic_name)
+    time.sleep(2)
+    content = requests.get(url).content
+    # with open("/Users/furuiyang/mydata/zhihupic/{}".format(pic_name), "wb") as f:
+    with open("/Users/furuiyang/Desktop/zhihu/{}".format(pic_name), "wb") as f:
+        f.write(content)
+
+
+def crawl(offset):
+    url = 'https://www.zhihu.com/api/v4/questions/363414427/answers?include=data%5B*%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%2Cis_recognized%2Cpaid_info%2Cpaid_info_content%3Bdata%5B*%5D.mark_infos%5B*%5D.url%3Bdata%5B*%5D.author.follower_count%2Cbadge%5B*%5D.topics' \
+          '&offset={}&limit=20&sort_by=updated'.format(offset)
+    r = requests.get(url, verify=False, headers=headers)
+    content = r.content.decode("utf-8")
+    ret = json.loads(content)
+    answers = ret.get("data")
+    for answer in answers:
+        content = answer.get("content")
+        doc = html.fromstring(content)
+        imgs = doc.xpath("//img/@src")
+        imgs = [img for img in imgs if img.startswith("http")]
+        if not imgs:
+            continue
+        for img in imgs:
+            download_picture(img)
+
+
+def main():
+    for i in range(1):
+        crawl(i*20)
+
+
+def t_test():
+    url = 'https://pic2.zhimg.com/50/v2-eef034a57bcab90d62bb1442bb52527c_hd.jpg'
+    content = requests.get(url).content
+    with open('temp.jpg', "wb") as f:
+        f.write(content)
+
+
+if __name__ == "__main__":
+    # main()
+
+    t_test()
